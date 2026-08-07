816d0a5dca0b33d7d078c343e634902a486b533f
rwang181-oss <rwang181@gmail.com>
2026-08-07 12:20:06 +0800

    feat: start automatic web version monitoring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,16 @@
 import '@/src/utils/i18n';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { startVersionMonitor } from '@/src/utils/versionMonitor';
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS !== 'web') return undefined;
+    return startVersionMonitor();
+  }, []);
+
   return (
     <>
       <StatusBar style="dark" />
